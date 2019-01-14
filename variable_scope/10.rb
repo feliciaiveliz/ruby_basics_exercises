4e4eaf819041de7f1d1067e0fdcaa4554e599143
feliciaiveliz <feliciaiveliz@gmail.com>
2019-01-14 18:13:45 +0000

    All exercises for the Variable Scope section

---
+++ variable_scope/10.rb
@@ -0,0 +1,13 @@
+a = 7
+array = [1, 2, 3]
+
+def my_value(ary)
+  ary.each do |b|
+    a += b
+  end
+end
+
+my_value(array)
+puts a 
+# undefined method `+' for nil:NilClass (NoMethodError)
+# the local variable 'a' outside of the method definition is invisible with respect to the each method. 
